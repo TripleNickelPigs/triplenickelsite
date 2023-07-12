15aeda0211c0c3e404fb6915a2345a02736a5315
Charles Panter <squidy.p@gmail.com>
2023-07-12 11:48:26 -0700

    update and add decapCMS

--- themes/tnp/assets/js/script.js
+++ themes/tnp/assets/js/script.js
@@ -1,18 +1,23 @@
+import CMS from "netlify-cms-app";
+// Initialize the CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate("my-template", MyTemplate);
+
 // Preloader js
-$(window).on('load', function () {
-	$('.preloader').addClass('d-none');
+$(window).on("load", function () {
+  $(".preloader").addClass("d-none");
 });
 
-(function($) {
-	'use strict';
+(function ($) {
+  "use strict";
 
-	//slider
-	$('.slider').not('.slick-initialized').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		dots: true,
-		arrows: false
-	});
-
+  //slider
+  $(".slider").not(".slick-initialized").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots: true,
+    arrows: false,
+  });
 })(jQuery);
